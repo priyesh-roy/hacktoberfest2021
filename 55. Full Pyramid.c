2161fb414c3582d95e7224a571d81d8492f8a750
mrroy2 <91710817+mrroy2@users.noreply.github.com>
2021-10-01 10:17:20 +0530

    Update 55. Full Pyramid.c

--- 55. Full Pyramid.c
+++ 55. Full Pyramid.c
@@ -2,7 +2,7 @@
 int main() {
    int i, j, n, k = 0;
    printf("Enter the number of rows: ");
-   scanf("&d", &n);
+   scanf("%d", &n);
    for (i = 1; i <= n; ++i) {
       for (j = 1; j <= n - i; ++j) {
          printf("  ");
