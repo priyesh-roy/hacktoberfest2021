e17c07a179955491cf8cf8daf0e1425bd24eea17
mrroy2 <91710817+mrroy2@users.noreply.github.com>
2021-10-01 10:15:49 +0530

    Update 52. Hollow Rectangle Pattern.c

--- 52. Hollow Rectangle Pattern.c
+++ 52. Hollow Rectangle Pattern.c
@@ -5,10 +5,10 @@ int main()
     int i, j, m, n;
      
     printf("Enter the Number of rows:\n");
-    scanf("&d", &m);
+    scanf("%d", &m);
     
     printf("Enter the Number of Columns:\n");
-    scanf("&d", &n);
+    scanf("%d", &n);
 	 
     for(i = 1; i <= m; i++)
     {
